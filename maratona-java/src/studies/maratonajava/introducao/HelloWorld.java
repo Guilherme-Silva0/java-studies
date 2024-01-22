fdfc2cd4c8a18d5545a5432f19b4b271ef84ca50
Guilherme-Silva0 <guilhermesilvadev0@gmail.com>
2024-01-22 15:35:06 -0300

    Comentários

--- maratona-java/src/studies/maratonajava/introducao/HelloWorld.java
+++ maratona-java/src/studies/maratonajava/introducao/HelloWorld.java
@@ -1,8 +1,17 @@
 package studies.maratonajava.introducao;
 
+/**
+ * Isso é um comentário <b>Javadoc</b>
+ */
+
 public class HelloWorld {
+    // Isso é um comentário de uma linha
+    /*
+        Isso é um comentário
+        de multiplas
+        linhas
+    */
     public static void main (String[] args) {
         System.out.println("Hello world");
-
     }
 }
