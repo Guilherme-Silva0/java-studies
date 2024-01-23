38b693dd26937a5cbba95263a8c26be2405fef98
Guilherme-Silva0 <guilhermesilvadev0@gmail.com>
2024-01-23 20:38:53 -0300

    convenções de variáveis

---
+++ maratona-java/src/studies/maratonajava/introducao/Aula02TiposPrimitivos.java
@@ -0,0 +1,9 @@
+package studies.maratonajava.introducao;
+
+public class Aula02TiposPrimitivos {
+    public static void main (String[] args) {
+        // int, double, float, char, byte, short, long, boolean
+        int age = 19;
+        System.out.println("Age is " + age + "years");
+    }
+}
