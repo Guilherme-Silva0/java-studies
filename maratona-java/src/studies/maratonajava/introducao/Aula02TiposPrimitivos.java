4d113aa94c11b845bfdfbb2331c0cacac002a3dd
Guilherme-Silva0 <guilhermesilvadev0@gmail.com>
2024-01-24 17:07:37 -0300

    Casting

--- maratona-java/src/studies/maratonajava/introducao/Aula02TiposPrimitivos.java
+++ maratona-java/src/studies/maratonajava/introducao/Aula02TiposPrimitivos.java
@@ -3,18 +3,21 @@ package studies.maratonajava.introducao;
 public class Aula02TiposPrimitivos {
     public static void main (String[] args) {
         // int, double, float, char, byte, short, long, boolean
-        int age = 19; // 4 bytes
-        long largeNumber = 100000; // 8 bytes
-        double wageDouble = 2000; // 8 bytes
-        float wageFloat = 2500; // 4 bytes
-        byte ageByte = 10; // 1 byte
-        short ageShort = 10; // 2 bytes
+        int age = 2147483647; // 4 bytes
+        long largeNumber = 2147483647; // 8 bytes
+        double wageDouble = 2000.0d; // 8 bytes
+        float wageFloat = 2500.0f; // 4 bytes
+        byte ageByte = 127; // 1 byte
+        short ageShort = 32767; // 2 bytes
         boolean trueVar = true; // 1 bit true or false
         boolean falseVar = false; // 1 bit true or false
         char character = 'M'; // 2 bytes
 
+        short castingNumber = (short) 10000000L; //Casting
+
         System.out.println("Age is " + age + " years");
         System.out.println(trueVar);
         System.out.println(character);
+        System.out.println(castingNumber);
     }
 }
