5e5fdae103b518758d94d0392afc6216669e8239
Guilherme-Silva0 <guilhermesilvadev0@gmail.com>
2024-01-25 17:44:51 -0300

    Aritiméticos

---
+++ maratona-java/src/studies/maratonajava/introducao/Aula04Operadores.java
@@ -0,0 +1,17 @@
+package studies.maratonajava.introducao;
+
+public class Aula04Operadores {
+    public static void main(String[] args) {
+        // + - / *
+
+        // O resulto da divisão entre dois números inteiros é sempre um numero inteiro.
+
+        int num1 = 10;
+        int num2 = 20;
+
+        System.out.println("Soma: " + ( num1 + num2 ));
+        System.out.println("Subtração: " + ( num1 - num2 ));
+        System.out.println("Multiplicação: " + ( num1 * num2 ));
+        System.out.println("Divisão: " + ( num1 / (double) num2 ));
+    }
+}
