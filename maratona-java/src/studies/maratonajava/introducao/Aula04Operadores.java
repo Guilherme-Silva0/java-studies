44c24d8fa8ee1140b7a3b11fbf469f13f380578a
Guilherme-Silva0 <guilhermesilvadev0@gmail.com>
2024-01-25 18:06:23 -0300

    Relacionais

--- maratona-java/src/studies/maratonajava/introducao/Aula04Operadores.java
+++ maratona-java/src/studies/maratonajava/introducao/Aula04Operadores.java
@@ -13,5 +13,24 @@ public class Aula04Operadores {
         System.out.println("Subtração: " + ( num1 - num2 ));
         System.out.println("Multiplicação: " + ( num1 * num2 ));
         System.out.println("Divisão: " + ( num1 / (double) num2 ));
+
+        System.out.println("\n");
+        // %
+
+        int rest = 20 % 7;
+
+        System.out.println("Resto da divisão: " + rest);
+
+        System.out.println("\n");
+        // < > <= >= == !=
+        boolean isTenGreaterThanTwenty = 10 > 20;
+        boolean isTenLessThanTwenty = 10 < 20;
+        boolean isTenEqualToTwenty = 10 == 20.0;
+        boolean isTenDifferentFromTwenty = 10 != 20.0;
+
+        System.out.println("10 é maior que 20? "+isTenGreaterThanTwenty);
+        System.out.println("10 é menor que 20? "+isTenLessThanTwenty);
+        System.out.println("10 é igual a 20? "+isTenEqualToTwenty);
+        System.out.println("10 é diferente de 20? "+isTenDifferentFromTwenty);
     }
 }
