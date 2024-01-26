8a35345ff5a62188b9c174513406df72ccb7d0b5
Guilherme-Silva0 <guilhermesilvadev0@gmail.com>
2024-01-26 20:23:44 -0300

    Operador ternário

---
+++ maratona-java/src/studies/maratonajava/introducao/Aula05EstruturasCondicionais03.java
@@ -0,0 +1,10 @@
+package studies.maratonajava.introducao;
+
+public class Aula05EstruturasCondicionais03 {
+    public static void main(String[] args) {
+        double wage = 6000;
+        String message = wage >= 5000 ? "Are able to donate" : "Unable to donate";
+
+        System.out.println(message);
+    }
+}
