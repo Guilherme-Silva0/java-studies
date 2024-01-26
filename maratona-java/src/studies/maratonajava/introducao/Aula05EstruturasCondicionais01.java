5f2c21ecf33a303f1a8b68bc709cdfaad3b18308
Guilherme-Silva0 <guilhermesilvadev0@gmail.com>
2024-01-26 19:27:53 -0300

    else

--- maratona-java/src/studies/maratonajava/introducao/Aula05EstruturasCondicionais01.java
+++ maratona-java/src/studies/maratonajava/introducao/Aula05EstruturasCondicionais01.java
@@ -1,16 +1,18 @@
 package studies.maratonajava.introducao;
 
-public class Aula05EstruturasCondicionais {
+public class Aula05EstruturasCondicionais01 {
     public static void main(String[] args) {
         int age = 20;
         boolean isOfLegalAge = age >= 18;
 
         if (isOfLegalAge) {
             System.out.println("Is authorized");
+        } else {
+            System.out.println("Not authorized");
         }
 
         if (!isOfLegalAge) {
-            System.out.println("Not authorized");
+            System.out.println("Is Minor");
         }
 
         System.out.println("Thanks!");
