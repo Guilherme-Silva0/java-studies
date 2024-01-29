91bcfdf556a50a4115a2115805534cab6cdb8acd
Guilherme-Silva0 <guilhermesilvadev0@gmail.com>
2024-01-29 16:26:22 -0300

    Laços de repetição while, do while, for

---
+++ maratona-java/src/studies/maratonajava/introducao/Aula05EstruturasDeRepeticao01.java
@@ -0,0 +1,27 @@
+package studies.maratonajava.introducao;
+
+public class Aula05EstruturasDeRepeticao01 {
+    public static void main(String[] args) {
+        // while, do while, for
+        int count = 0;
+
+        System.out.println("While");
+
+        while (count <= 10) {
+            System.out.println(count++);
+        }
+
+        System.out.println("\nDo While");
+        count = 0;
+
+        do {
+            System.out.println(count++);
+        } while (count <= 10);
+
+        System.out.println("\nFor");
+
+        for (int i = 0; i <= 10; i++) {
+            System.out.println(i);
+        }
+    }
+}
