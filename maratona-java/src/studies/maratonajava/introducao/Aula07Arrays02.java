193efd41a7999570ce74785f5c758d896fdbece4
Guilherme-Silva0 <guilhermesilvadev0@gmail.com>
2024-02-01 15:59:11 -0300

    Arrays pt 2

---
+++ maratona-java/src/studies/maratonajava/introducao/Aula07Arrays02.java
@@ -0,0 +1,13 @@
+package studies.maratonajava.introducao;
+
+public class Aula07Arrays02 {
+    public static void main(String[] args) {
+        // Byte, short, int, long, float, e double 0
+        // char '\u0000'
+        // boolean false
+        // String null
+
+        String[] names = new String[3];
+        System.out.println(names[0] + " " + names[1] + " " + names[2]);
+    }
+}
