1611a7cac7f67b88620b0a3e857abb37a9152f5f
Guilherme-Silva0 <guilhermesilvadev0@gmail.com>
2024-02-01 15:52:46 -0300

    Arrays pt 1

---
+++ maratona-java/src/studies/maratonajava/introducao/Aula07Arrays01.java
@@ -0,0 +1,12 @@
+package studies.maratonajava.introducao;
+
+public class Aula07Arrays01 {
+    public static void main(String[] args) {
+        int[] ages = new int[3];
+        ages[0] = 23;
+        ages[1] = 28;
+        ages[2] = 29;
+
+        System.out.println(ages[0] + " " + ages[1] + " " + ages[2]);
+    }
+}
