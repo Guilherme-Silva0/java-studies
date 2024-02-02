a1c0ed56ea9b29ec5fbbb3cf1bf9831a63e94e2f
Guilherme-Silva0 <guilhermesilvadev0@gmail.com>
2024-02-02 19:30:38 -0300

    Introdução classes pt 02

---
+++ maratona-java/src/studies/maratonajava/javacore/Aintroducaoclasses/test/EstudanteTest02.java
@@ -0,0 +1,12 @@
+package studies.maratonajava.javacore.Aintroducaoclasses.test;
+
+import studies.maratonajava.javacore.Aintroducaoclasses.model.Estudante;
+
+public class EstudanteTest02 {
+    public static void main(String[] args) {
+        Estudante estudante = new Estudante();
+        System.out.println(estudante.nome);
+        System.out.println(estudante.idade);
+        System.out.println(estudante.sexo);
+    }
+}
