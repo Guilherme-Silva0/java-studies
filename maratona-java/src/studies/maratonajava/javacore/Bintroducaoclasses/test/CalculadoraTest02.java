1d5a64087352b26af5819f3f423574fbd4eae004
Guilherme-Silva0 <guilhermesilvadev0@gmail.com>
2024-02-07 18:25:22 -0300

    Parâmetros

---
+++ maratona-java/src/studies/maratonajava/javacore/Bintroducaoclasses/test/CalculadoraTest02.java
@@ -0,0 +1,11 @@
+package studies.maratonajava.javacore.Bintroducaoclasses.test;
+
+import studies.maratonajava.javacore.Bintroducaoclasses.model.Calculadora;
+
+public class CalculadoraTest02 {
+    public static void main(String[] args) {
+        Calculadora calculadora = new Calculadora();
+
+        calculadora.multiplicaDoisNumeros(2, 6);
+    }
+}
