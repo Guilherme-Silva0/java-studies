3ee38512798ca429c7035bae5801d8006df2d2ef
Guilherme-Silva0 <guilhermesilvadev0@gmail.com>
2024-02-20 14:22:25 -0300

    Construtores pt 02 - Sobrecarga

--- maratona-java/src/studies/maratonajava/javacore/DConstrutores/test/AnimeTest01.java
+++ maratona-java/src/studies/maratonajava/javacore/DConstrutores/test/AnimeTest01.java
@@ -4,9 +4,7 @@ import studies.maratonajava.javacore.DConstrutores.model.Anime;
 
 public class AnimeTest01 {
     public static void main(String[] args) {
-        Anime anime = new Anime("One Piece", "TV", 1102, "Ação");
-        Anime anime1 = new Anime();
+        Anime anime = new Anime("One Piece", "TV", 1102, "Ação", "WIT Studio");
         anime.imprimir();
-        anime1.imprimir();
     }
 }
