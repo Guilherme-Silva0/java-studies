f43d1703728a5df06d6adbf03a162de3193cd0a0
Guilherme-Silva0 <guilhermesilvadev0@gmail.com>
2024-02-29 15:02:28 -0300

    Exercício leitura de dados do teclado

---
+++ maratona-java/src/studies/maratonajava/javacore/Gassociacao/test/LeituraTeclas02.java
@@ -0,0 +1,21 @@
+package studies.maratonajava.javacore.Gassociacao.test;
+
+import java.util.Scanner;
+
+public class LeituraTeclas02 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("----- O grande software de previsão do futuro -----");
+        System.out.println();
+
+        System.out.print("Digite sua pergunta e eu responderei sim ou não: ");
+        String pergunta = input.nextLine();
+
+        if (pergunta.charAt(0) == ' ') {
+            System.out.println("SIM");
+        } else {
+            System.out.println("Não");
+        }
+    }
+}
