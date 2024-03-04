59ff78478ff43dfa9a0308d6f0012275ed5ac935
Guilherme-Silva0 <guilhermesilvadev0@gmail.com>
2024-03-04 19:08:08 -0300

    Herança pt 05 - Sequência de inicialização

--- maratona-java/src/studies/maratonajava/javacore/Hheranca/model/Funcionario.java
+++ maratona-java/src/studies/maratonajava/javacore/Hheranca/model/Funcionario.java
@@ -3,8 +3,21 @@ package studies.maratonajava.javacore.Hheranca.model;
 public class Funcionario extends Pessoa {
     private double salario;
 
+    static {
+        System.out.println("Dentro do bloco de inicialização estático do funcionario");
+    }
+
+    {
+        System.out.println("Dentro do bloco de inicialização não estático 1 do funcionario");
+    }
+
+    {
+        System.out.println("Dentro do bloco de inicialização não estático 2 do funcionario");
+    }
+
     public Funcionario(String nome) {
         super(nome);
+        System.out.println("Dentro do contrutor de funcionario");
     }
 
     public void imprime() {
---
+++ maratona-java/src/studies/maratonajava/javacore/Hheranca/test/HerancaTest02.java
@@ -0,0 +1,9 @@
+package studies.maratonajava.javacore.Hheranca.test;
+
+import studies.maratonajava.javacore.Hheranca.model.Funcionario;
+
+public class HerancaTest02 {
+    public static void main(String[] args) {
+        Funcionario funcionario = new Funcionario("Roberto");
+    }
+}
