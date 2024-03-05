9537f84138d5e62abe8aa55f6d8be10af04bad12
Guilherme-Silva0 <guilhermesilvadev0@gmail.com>
2024-03-05 20:16:38 -0300

    Sobrescrita do método toString

---
+++ maratona-java/src/studies/maratonajava/javacore/Isobrescrita/test/AnimeTest01.java
@@ -0,0 +1,10 @@
+package studies.maratonajava.javacore.Isobrescrita.test;
+
+import studies.maratonajava.javacore.Isobrescrita.model.Anime;
+
+public class AnimeTest01 {
+    public static void main(String[] args) {
+        Anime anime = new Anime("Dr. Stone");
+        System.out.println(anime);
+    }
+}
