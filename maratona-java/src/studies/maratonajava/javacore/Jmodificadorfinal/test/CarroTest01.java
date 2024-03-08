23274e5112941c60cfe13eae7a0277a3d5c3dc98
Guilherme-Silva0 <guilhermesilvadev0@gmail.com>
2024-03-07 21:02:02 -0300

    Alterando valor

--- maratona-java/src/studies/maratonajava/javacore/Jmodificadorfinal/test/CarroTest01.java
+++ maratona-java/src/studies/maratonajava/javacore/Jmodificadorfinal/test/CarroTest01.java
@@ -5,7 +5,7 @@ import studies.maratonajava.javacore.Jmodificadorfinal.model.Carro;
 public class CarroTest01 {
     public static void main(String[] args) {
         Carro carro = new Carro();
-        carro.setNome("Fusca");
+        carro.setNome("Vectar");
         System.out.println(Carro.VELOCIDADE_LIMITE + " " + carro.getNome());
     }
 }
