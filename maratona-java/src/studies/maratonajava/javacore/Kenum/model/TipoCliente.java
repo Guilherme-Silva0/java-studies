1f5a4ed5d8c096a6c97b74406a1c5e4dfc14da0b
Guilherme-Silva0 <guilhermesilvadev0@gmail.com>
2024-03-18 22:31:17 -0300

    Enumeração pt 01 - Introdução

---
+++ maratona-java/src/studies/maratonajava/javacore/Kenum/model/TipoCliente.java
@@ -0,0 +1,6 @@
+package studies.maratonajava.javacore.Kenum.model;
+
+public enum TipoCliente {
+    PESSOA_FISICA,
+    PESSOA_JURIDICA
+}
---
+++ maratona-java/src/studies/maratonajava/javacore/Kenum/test/ClienteTest01.java
@@ -0,0 +1,14 @@
+package studies.maratonajava.javacore.Kenum.test;
+
+import studies.maratonajava.javacore.Kenum.model.Cliente;
+import studies.maratonajava.javacore.Kenum.model.TipoCliente;
+
+public class ClienteTest01 {
+    public static void main(String[] args) {
+        Cliente cliente01 = new Cliente("Amanda", TipoCliente.PESSOA_FISICA);
+        Cliente cliente02 = new Cliente("Pedro", TipoCliente.PESSOA_JURIDICA);
+
+        System.out.println(cliente01);
+        System.out.println(cliente02);
+    }
+}
