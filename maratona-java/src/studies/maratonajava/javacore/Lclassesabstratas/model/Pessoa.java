74ad9a09a39069781e3b2515c6be6107a30fb315
Guilherme-Silva0 <guilhermesilvadev0@gmail.com>
2024-03-22 11:40:32 -0300

    Classes abstratas pt 03 - Métodos abstratos regras

---
+++ maratona-java/src/studies/maratonajava/javacore/Lclassesabstratas/model/Pessoa.java
@@ -0,0 +1,5 @@
+package studies.maratonajava.javacore.Lclassesabstratas.model;
+
+public abstract class Pessoa {
+    public abstract void imprime();
+}
--- maratona-java/src/studies/maratonajava/javacore/Lclassesabstratas/test/FuncionarioTest01.java
+++ maratona-java/src/studies/maratonajava/javacore/Lclassesabstratas/test/FuncionarioTest01.java
@@ -9,5 +9,7 @@ public class FuncionarioTest01 {
         Desenvolvedor desenvolvedor = new Desenvolvedor("Guilherme", 9000);
         System.out.println(gerente);
         System.out.println(desenvolvedor);
+        gerente.imprime();
+        desenvolvedor.imprime();
     }
 }
