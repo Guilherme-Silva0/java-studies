9c6d2f49b9e7421669e9e4a17ce58e7dadb4610e
Guilherme-Silva0 <guilhermesilvadev0@gmail.com>
2024-03-22 11:29:35 -0300

    Classes abstratas pt 02 - Métodos abstratos

--- maratona-java/src/studies/maratonajava/javacore/Lclassesabstratas/model/Desenvolvedor.java
+++ maratona-java/src/studies/maratonajava/javacore/Lclassesabstratas/model/Desenvolvedor.java
@@ -6,6 +6,11 @@ public class Desenvolvedor extends Funcionario {
     }
 
     @Override
+    public void calculaBonus() {
+        this.salario *= 1.05;
+    }
+
+    @Override
     public String toString() {
         return "Desenvolvedor{" +
                 "nome='" + nome + '\'' +
--- maratona-java/src/studies/maratonajava/javacore/Lclassesabstratas/test/FuncionarioTest01.java
+++ maratona-java/src/studies/maratonajava/javacore/Lclassesabstratas/test/FuncionarioTest01.java
@@ -5,7 +5,7 @@ import studies.maratonajava.javacore.Lclassesabstratas.model.Gerente;
 
 public class FuncionarioTest01 {
     public static void main(String[] args) {
-        Gerente gerente = new Gerente("Luffy", 9000);
+        Gerente gerente = new Gerente("Luffy", 1100);
         Desenvolvedor desenvolvedor = new Desenvolvedor("Guilherme", 9000);
         System.out.println(gerente);
         System.out.println(desenvolvedor);
