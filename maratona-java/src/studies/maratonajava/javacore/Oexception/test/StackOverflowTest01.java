bb0743818459caca4167e2c7a9c4b2a391a558d3
Guilherme-Silva0 <guilhermesilvadev0@gmail.com>
2024-04-01 19:13:43 -0300

    Exceções pt 01 - Errors

---
+++ maratona-java/src/studies/maratonajava/javacore/Oexception/test/StackOverflowTest01.java
@@ -0,0 +1,11 @@
+package studies.maratonajava.javacore.Oexception.test;
+
+public class StackOverflowTest01 {
+    public static void main(String[] args) {
+        recursion();
+    }
+
+    public static void recursion() {
+        recursion();
+    }
+}
