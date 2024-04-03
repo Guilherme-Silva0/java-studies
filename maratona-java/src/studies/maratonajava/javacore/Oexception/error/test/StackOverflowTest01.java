513ce999f7442e76b7187101ac86bcc00ddb7aff
Guilherme-Silva0 <guilhermesilvadev0@gmail.com>
2024-04-02 22:53:46 -0300

    Exceções pt 02 - RuntimeException

--- maratona-java/src/studies/maratonajava/javacore/Oexception/error/test/StackOverflowTest01.java
+++ maratona-java/src/studies/maratonajava/javacore/Oexception/error/test/StackOverflowTest01.java
@@ -1,4 +1,4 @@
-package studies.maratonajava.javacore.Oexception.test;
+package studies.maratonajava.javacore.Oexception.error.test;
 
 public class StackOverflowTest01 {
     public static void main(String[] args) {
---
+++ maratona-java/src/studies/maratonajava/javacore/Oexception/runtime/test/RuntimeExceptionTest01.java
@@ -0,0 +1,9 @@
+package studies.maratonajava.javacore.Oexception.runtime.test;
+
+public class RuntimeExceptionTest01 {
+    public static void main(String[] args) {
+        //Checked and Unckecked
+        int[] nums = {1, 2};
+        System.out.println(nums[2]);
+    }
+}
