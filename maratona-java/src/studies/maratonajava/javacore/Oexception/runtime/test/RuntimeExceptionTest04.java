1b3160d3c5aa88de14c3ec4f9e1372069a313e98
Guilherme-Silva0 <guilhermesilvadev0@gmail.com>
2024-04-04 16:23:29 -0300

    Exceções pt 07 - Capturando múltiplas exceções

---
+++ maratona-java/src/studies/maratonajava/javacore/Oexception/runtime/test/RuntimeExceptionTest04.java
@@ -0,0 +1,35 @@
+package studies.maratonajava.javacore.Oexception.runtime.test;
+
+import java.io.FileNotFoundException;
+import java.sql.SQLException;
+
+public class RuntimeExceptionTest04 {
+    public static void main(String[] args) {
+        try {
+            throw new RuntimeException();
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Inside of ArrayIndexOutOfBoundsException");
+        } catch (IndexOutOfBoundsException e) {
+            System.out.println("Inside of IndexOutOfBoundsException");
+        } catch (IllegalArgumentException e) {
+            System.out.println("Inside of IllegalArgumentException");
+        } catch (ArithmeticException e) {
+            System.out.println("Inside of ArithmeticException");
+        } catch (RuntimeException e) {
+            System.out.println("Inside of RuntimeException");
+        } catch (Exception e) {
+            System.out.println("Inside of Exception");
+        }
+
+        try {
+            maybeThrowException();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void maybeThrowException() throws SQLException, FileNotFoundException {
+    }
+}
