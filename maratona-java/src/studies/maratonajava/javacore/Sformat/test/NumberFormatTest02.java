5579ee969b3de9b78f3041782c993fe553663e38
Guilherme-Silva0 <guilhermesilvadev0@gmail.com>
2024-04-08 14:04:37 -0300

    Classes Utilitárias - Internacionalização de moeda com Locale

--- maratona-java/src/studies/maratonajava/javacore/Sformat/test/NumberFormatTest02.java
+++ maratona-java/src/studies/maratonajava/javacore/Sformat/test/NumberFormatTest02.java
@@ -4,28 +4,29 @@ import java.text.NumberFormat;
 import java.text.ParseException;
 import java.util.Locale;
 
-public class NumberFormatTest01 {
+public class NumberFormatTest02 {
     public static void main(String[] args) {
         Locale localeBR = Locale.of("pt", "BR");
+        Locale localeEUA = Locale.US;
         Locale localeJP = Locale.JAPAN;
         Locale localeIT = Locale.ITALY;
 
         NumberFormat[] numberFormats = {
-                NumberFormat.getInstance(),
-                NumberFormat.getInstance(localeBR),
-                NumberFormat.getInstance(localeJP),
-                NumberFormat.getInstance(localeIT)
+                NumberFormat.getCurrencyInstance(),
+                NumberFormat.getCurrencyInstance(localeBR),
+                NumberFormat.getCurrencyInstance(localeEUA),
+                NumberFormat.getCurrencyInstance(localeJP),
+                NumberFormat.getCurrencyInstance(localeIT)
         };
 
-        double value = 1000.2130;
+        double value = 100000000.2130;
         for (NumberFormat numberFormat : numberFormats) {
-            numberFormat.setMaximumFractionDigits(2);
             System.out.println(numberFormat.format(value));
         }
 
-        String stringValue = "1000.2130";
+        String stringValue = "￥1000";
         try {
-            System.out.println(numberFormats[1].parse(stringValue));
+            System.out.println(numberFormats[3].parse(stringValue));
         } catch (ParseException e) {
             e.printStackTrace(System.out);
         }
