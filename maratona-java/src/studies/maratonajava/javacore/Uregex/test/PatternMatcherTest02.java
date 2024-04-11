3fad3e57ec454d5e44df1339be7ded4e91fa6b14
Guilherme-Silva0 <guilhermesilvadev0@gmail.com>
2024-04-11 14:59:58 -0300

    Classes Utilitárias - Regex pt 02 - Pattern e Matcher - Meta caracteres

--- maratona-java/src/studies/maratonajava/javacore/Uregex/test/PatternMatcherTest02.java
+++ maratona-java/src/studies/maratonajava/javacore/Uregex/test/PatternMatcherTest02.java
@@ -4,15 +4,22 @@ import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class PatternMatcherTest01 {
+public class PatternMatcherTest02 {
     public static void main(String[] args) {
+        // \d = All digits
+        // \D = Everything that isn't digits
+        // \s = Blank spaces  \t \n \f \r
+        // \S = All non-whitespace characters
+        // \w = All letters, numbers and underscore
+        // \W = Anything that is not letters, numbers or underscore
+        String regex = "\\w";
+
         Scanner input = new Scanner(System.in);
-
-        String regex = "ab";
-
         System.out.println("Regex: " + regex);
         System.out.print("Enter text: ");
         String text = input.next().trim();
+
+        System.out.println("Regex: " + regex);
 
         Pattern pattern = Pattern.compile(regex);
         Matcher matcher = pattern.matcher(text);
@@ -21,7 +28,7 @@ public class PatternMatcherTest01 {
 
         System.out.print("\nPositions found: ");
         while (matcher.find()) {
-            System.out.print(matcher.start() + " ");
+            System.out.print(matcher.start() + ": " + matcher.group() + "; ");
         }
     }
 }
