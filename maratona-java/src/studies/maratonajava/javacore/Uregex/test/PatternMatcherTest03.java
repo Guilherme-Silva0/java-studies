00c14342c0006649d1b5270dec2a5b494a923aac
Guilherme-Silva0 <guilhermesilvadev0@gmail.com>
2024-04-12 17:56:44 -0300

    Classes Utilitárias - Regex pt 03 - Pattern e Matcher - Range

---
+++ maratona-java/src/studies/maratonajava/javacore/Uregex/test/PatternMatcherTest03.java
@@ -0,0 +1,35 @@
+package studies.maratonajava.javacore.Uregex.test;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class PatternMatcherTest03 {
+    public static void main(String[] args) {
+        // \d = All digits
+        // \D = Everything that isn't digits
+        // \s = Blank spaces  \t \n \f \r
+        // \S = All non-whitespace characters
+        // \w = All letters, numbers and underscore
+        // \W = Anything that is not letters, numbers or underscore
+        // [] - Range
+//        String regex = "[a-zA-C]";
+        String regex = "0[xX][0-9a-fA-F]";
+
+        System.out.println("Regex: " + regex);
+        String text = "12 0x 0x 0xFFABC 0x109 0x1";
+
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(text);
+        System.out.println("Text:  " + text);
+        System.out.println("Index: 0123456789");
+
+        StringBuilder assembledText = new StringBuilder();
+
+        System.out.print("\nPositions found: ");
+        while (matcher.find()) {
+            System.out.print(matcher.start() + ": " + matcher.group() + "; ");
+            assembledText.append(matcher.group());
+        }
+        System.out.println("\nAssembled String: " + assembledText);
+    }
+}
