efc3cf82888f8ab0ed4f22fa9153e1d6285b9a44
Guilherme-Silva0 <guilhermesilvadev0@gmail.com>
2024-04-16 19:50:27 -0300

    Classes Utilitárias - NIO pt 01 - Path, Paths, Files pt 01

---
+++ maratona-java/src/studies/maratonajava/javacore/Wnio/test/PathTest01.java
@@ -0,0 +1,11 @@
+package studies.maratonajava.javacore.Wnio.test;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class PathTest01 {
+    public static void main(String[] args) {
+        Path p1 = Paths.get("files/test.txt");
+        System.out.println(p1.getFileName());
+    }
+}
