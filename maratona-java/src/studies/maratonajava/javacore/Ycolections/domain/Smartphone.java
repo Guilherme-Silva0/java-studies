24d7b8d897b82ef83a28a7745a90d1b6124e8898
Guilherme-Silva0 <guilhermesilvadev0@gmail.com>
2024-04-21 15:16:40 -0300

    161 - Coleções pt 01 - equals pt 01

---
+++ maratona-java/src/studies/maratonajava/javacore/Ycolections/domain/Smartphone.java
@@ -0,0 +1,27 @@
+package studies.maratonajava.javacore.Ycolections.domain;
+
+public class Smartphone {
+    private String serialNumber;
+    private String brand;
+
+    public Smartphone(String serialNumber, String brand) {
+        this.serialNumber = serialNumber;
+        this.brand = brand;
+    }
+
+    public String getSerialNumber() {
+        return serialNumber;
+    }
+
+    public void setSerialNumber(String serialNumber) {
+        this.serialNumber = serialNumber;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+}
