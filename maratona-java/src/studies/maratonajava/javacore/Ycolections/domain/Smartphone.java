f72681264ea73ab5faa7663913084a5a102ac26d
Guilherme-Silva0 <guilhermesilvadev0@gmail.com>
2024-04-23 13:58:51 -0300

    168 - Coleções pt 08 - List pt 03

--- maratona-java/src/studies/maratonajava/javacore/Ycolections/domain/Smartphone.java
+++ maratona-java/src/studies/maratonajava/javacore/Ycolections/domain/Smartphone.java
@@ -27,6 +27,14 @@ public class Smartphone {
         return this.serialNumber == null ? 0 : this.serialNumber.hashCode();
     }
 
+    @Override
+    public String toString() {
+        return "Smartphone{" +
+                "serialNumber='" + serialNumber + '\'' +
+                ", brand='" + brand + '\'' +
+                '}';
+    }
+
     public String getSerialNumber() {
         return serialNumber;
     }
