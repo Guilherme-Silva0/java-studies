2fce1ed123309d18762bfc33b2dfee61d9fffe1c
Guilherme-Silva0 <guilhermesilvadev0@gmail.com>
2024-04-30 19:59:41 -0300

    177 - Coleções pt 17 - NavigableSet, TreeSet pt 02

--- maratona-java/src/studies/maratonajava/javacore/Ycolections/test/NavigableSetTest01.java
+++ maratona-java/src/studies/maratonajava/javacore/Ycolections/test/NavigableSetTest01.java
@@ -14,13 +14,20 @@ class SmartphoneBrandComparator implements Comparator<Smartphone> {
     }
 }
 
+class MangaPriceComparator implements Comparator<Manga> {
+    @Override
+    public int compare(Manga manga1, Manga manga2) {
+        return manga1.getPrice().compareTo(manga2.getPrice());
+    }
+}
+
 public class NavigableSetTest01 {
     public static void main(String[] args) {
         NavigableSet<Smartphone> set = new TreeSet<>(new SmartphoneBrandComparator());
         Smartphone smartphone = new Smartphone("123", "Nokia");
         set.add(smartphone);
 
-        NavigableSet<Manga> mangas = new TreeSet<>();
+        NavigableSet<Manga> mangas = new TreeSet<>(new MangaByPriceComparator());
         mangas.add(new Manga(5L, "Berserk", 19.99, 0));
         mangas.add(new Manga(2L, "Hellsing Ultimate", 23.99, 5));
         mangas.add(new Manga(4L, "Attack on Titan", 25.99, 0));
@@ -28,8 +35,27 @@ public class NavigableSetTest01 {
         mangas.add(new Manga(3L, "Dragon Ball Z", 15.99, 0));
         mangas.add(new Manga(3L, "Dragon Ball Z", 15.99, 0));
 
-        for (Manga manga : mangas) {
+        for (Manga manga : mangas.descendingSet()) {
             System.out.println(manga);
         }
+
+        System.out.println("----------------------------");
+
+        Manga yuyu = new Manga(6L, "Yuyu Hakusho", 19.99, 5);
+        // lower <
+        System.out.println(mangas.lower(yuyu));
+        // floor <=
+        System.out.println(mangas.floor(yuyu));
+        // higher >
+        System.out.println(mangas.higher(yuyu));
+        // ceiling >=
+        System.out.println(mangas.ceiling(yuyu));
+
+        System.out.println("-------------------------");
+        System.out.println(mangas.size());
+        System.out.println(mangas.pollFirst());
+        System.out.println(mangas.size());
+        System.out.println(mangas.pollLast());
+        System.out.println(mangas.size());
     }
 }
